d4c149876a54601416e3d62fa7177d6392bc601d
Arda <gfs.apex@gmail.com>
2026-08-23 22:15:19 +0100

    Initial Commit

---
+++ AegisDesk/AegisDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AegisDeskApp.swift
+//  AegisDesk
+//
+//  Created by Arda Gungor on 23/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct AegisDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
